3664fab423de03ad78321ad981d982b17c1feddd
Bret Copeland <bret@atlantisflight.org>
2015-03-11 02:16:56 -0400

    Setup AppVeyor.

--- BitPacker/Properties/AssemblyInfo.cs
+++ BitPacker/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BitPacker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple binary serializer for fixed-sized types.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BitPacker")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
